7c5668ce9b6aee5a391732b1ce30499cda291aa1
Wilfried Hounyo <wilfried.hounyo@gmail.com>
2016-07-18 23:46:08 -0400

    added post support to Pasta

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ HOSTNAME = "api.github.com"
 URL = ""
 NAME = ""
 DESCRIPTION = ""
+PRIVATE = True
 
 parser = argparse.ArgumentParser(description='Pastebin with Github Gists.')
 parser.add_argument('-r', '--read',help='Read from specific gist', action="store_true")
 parser.add_argument('-u','--url', help='Specific to read from or write to')
 parser.add_argument('-n', '--name', default='untitled.txt', help='Name for gist')
+parser.add_argument('-s', '--secret',help='Make the gist private', action="store_true")
 parser.add_argument('-d', '--desc', default='', help='Description of gist')
 
 def readgist():
@@ -22,15 +24,34 @@ def readgist():
     print(data)
 
 def paste():
-    print("Pasting to url: ", URL)
+    data_json = {
+      "description": DESCRIPTION,
+      "files": {
+        NAME: {
+          "content": ""
+        }
+      }
+    }
+    if not sys.stdin.isatty():
+        data = sys.stdin.readlines()
+        data_json["files"][NAME]["content"] = json.dumps(' '.join(data))
+        sys.stdin = open('/dev/tty')
+    else:
+        data = input("Input: ")
+        data_json["files"][NAME]["content"] = json.dumps(' '.join(data))
+    data_json["files"][NAME]["public"] = False if PRIVATE else True
+    data_json = json.dumps(data_json)
+    r = requests.post(URL, data=data_json)
+    print("Your pasta is available at: ", r.json()["html_url"])
 
 def geturl():
-    print("creating new url")
+    return "https://api.github.com/gists"
 
 if __name__ == "__main__":
     args = parser.parse_args()
     NAME = args.name
     DESCRIPTION = args.desc
+    PRIVATE = args.secret
     if args.url:
         URL = args.url
         if args.read:
